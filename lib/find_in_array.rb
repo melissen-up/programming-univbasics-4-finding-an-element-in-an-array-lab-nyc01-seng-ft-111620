cc001a31749b2d39b1e1d4cc520d26ecb0bb6f06
Melissa F <melissa.faulner@gmail.com>
2020-11-02 03:26:02 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while counter < array.length do 
-    if array.include?(value_to_find) == TRUE
-      puts 
+    puts array[counter] == value_to_find 
     counter += 1
   end
-  
 end
